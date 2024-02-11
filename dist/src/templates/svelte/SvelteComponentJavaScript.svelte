f1d0f5c751e9d9841ffef3d4b109cdfa3aaba38a
roybert35 <roybert35@gmail.com>
2024-02-11 12:52:08 -0500

    feat: updated version with templates

---
+++ dist/src/templates/svelte/SvelteComponentJavaScript.svelte
@@ -0,0 +1,8 @@
+<!-- SvelteComponent.svelte -->
+<script>
+  let message = "Hello from Svelte!";
+</script>
+
+<div>
+  <h1>{message}</h1>
+</div>
---
+++ dist/src/templates/svelte/SvelteComponentTypeScript.svelte
@@ -0,0 +1,8 @@
+<!-- SvelteComponent.svelte -->
+<script lang="ts">
+  let message: string = "Hello from Svelte!";
+</script>
+
+<div>
+  <h1>{message}</h1>
+</div>
